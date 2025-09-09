0da940e6ae731adc7e738c558d59daf521160503
Nishi Shah <nishahh26@gmail.com>
2025-09-09 10:52:43 +0530

    Create Que6.c

---
+++ Que6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a[2][2], b[2][2],c[2][2];
+    printf("enter 4 numbers for array a\n");
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++)
+            scanf("%d",&a[i][j]);}
+    printf("enter 4 numbers for array b\n");
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++)
+            scanf("%d",&b[i][j]);}
+    for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++)
+                c[i][j]= a[i][j]+ b[i][j];}
+    printf("the array c is:\n");
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++)
+            printf("%d\t",c[i][j]);
+        printf("\n");}
+    return 0;
+}
