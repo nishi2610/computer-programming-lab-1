1cff1d496b0457242d15b85059de3b6ac03f0905
Nishi Shah <nishahh26@gmail.com>
2025-09-09 10:23:19 +0530

    Create Que2.c

---
+++ Que2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a[5];
+    printf("enter number\n");
+    for(int i=0;i<5;i++)
+        scanf("%d/n",&a[i]);
+    for(int i=0;i<5;i++)
+        printf("%d\n",a[i]);
+    return 0;
+}
