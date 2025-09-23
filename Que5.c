7323e97d10cd12e4feccd56c44cc16b4e6a7a068
Nishi Shah <nishahh26@gmail.com>
2025-09-23 09:58:34 +0530

    Update Que5.c

--- Que5.c
+++ Que5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    int a[2][2], b[2][2],c[2][2];
-    printf("enter 4 numbers for array a\n");
+    int a[2][2], b[2][2], c[2][2];
+    printf("Enter 4 numbers for array a\n");
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++)
             scanf("%d",&a[i][j]);}
