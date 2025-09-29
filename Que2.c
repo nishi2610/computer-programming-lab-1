befea73abd051819261fb4f0fd0aa97d206434ed
Nishi Shah <nishahh26@gmail.com>
2025-09-29 23:06:11 +0530

    Update Que2.c

--- Que2.c
+++ Que2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-int main()
-{
-    int a[5];
-    printf("enter number\n");
-    for(int i=0;i<5;i++)
-        scanf("%d/n",&a[i]);
-    for(int i=0;i<5;i++)
-        printf("%d\n",a[i]);
-    return 0;
+int main() {
+int a[10];
+printf("Enter 10 values:\n");
+for (int i = 0; i < 10; i++) {
+    printf("Enter value %d: ", i + 1);
+    scanf("%d", &a[i]);}
+printf("The 4th value you entered is: %d\n", a[3]);
+printf("The 7th value you entered is: %d\n", a[6]);
+printf("The 9th value you entered is: %d\n", a[8]);
+return 0;
 }
