edcde0da1c7a37d3f2a610e426d013a96f3abb5a
Nishi Shah <nishahh26@gmail.com>
2025-11-10 22:44:08 +0530

    Create strcopy.c

---
+++ strcopy.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char str1[100],str2[100];
+printf("Enter string: ");
+gets(str1);
+strcpy(str2,str1);
+printf("The copied string is:  %s\n", str2);
+return 0;
+}
