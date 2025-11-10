3d1de49ea490923521dc55da8fedd0127c6e846d
Nishi Shah <nishahh26@gmail.com>
2025-11-10 23:01:18 +0530

    Create ReverseVertical.c

---
+++ ReverseVertical.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char str[100];
+printf("Enter the string: ");
+gets(str);
+strrev(str);
+printf("the reverse string in vertical is:\n");
+for(int i=0;i<strlen(str);i++){
+    printf("%c\n",str[i]);}
+return 0;
+}
